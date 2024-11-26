2409250fa6d3901e1b77b77a4bcf090bd073028a
Saeed Mahud Rahat <118961502+rahatdevops@users.noreply.github.com>
2024-11-26 21:57:16 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From rahatgithub" }');
+    res.send('{ "response": "Hello From rahatgithub-v2" }');
 });
 
 app.get('/will', function (req, res) {
